fb2485f6e35727de4b4bb340e439626e6a163728
hoynhan <38449097+hoynhan@users.noreply.github.com>
2020-04-12 16:47:28 +0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
